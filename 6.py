3a0623aa2a4f3bf09fd66c08cef6aacabee9abfe
“glebbutyaev” <bgpyth94@gmail.com>
2021-01-28 14:18:33 -0800

    add 6.py.

---
+++ 6.py
@@ -0,0 +1,16 @@
+                                                                                #Простые числа (6)
+
+#Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000, и возвращающую True, если оно простое, и False - иначе
+
+
+
+def is_prime(x):
+    if x == 0 or x == 1:
+        return("False")
+    for i in range (2, x):
+        if x % i == 0:
+            return("False")
+
+    return("True")
+
+is_prime(997)
